f82793901389ea0dd8ba3fa10071aaa54695d7e3
Takeshi Kawai <takezou621@gmail.com>
2025-07-13 15:29:55 +0900

    feat: Add calculator with add and multiply functions (closes #1)

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    """Add two numbers"""
+    return a + b
+
+def multiply(a, b):
+    """Multiply two numbers"""
+    return a * b
+
+if __name__ == '__main__':
+    print('Calculator functions ready\!')
+    print(f'2 + 3 = {add(2, 3)}')
+    print(f'4 * 5 = {multiply(4, 5)}')
